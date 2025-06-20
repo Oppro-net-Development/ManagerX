55d267fad2cb65d63ff5c209a8eb3ce9cae12e8d
LennyPegauOfficial <oppro.netzwerk@gmail.com>
2025-06-20 22:29:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import discord
-from discord.ext import commands, tasks
-from discord import slash_command
 import os
-import random
 from dotenv import load_dotenv
 
+from datetime import datetime
+from colorama import Fore, Style
+import asyncio
 import ezcord
 import logging
 
-
+from FastCoding.backend import init_all
 intents = discord.Intents.default()
 intents.members = True
 intents.guilds = True
 intents.messages = True
 load_dotenv()
+time = datetime.now().strftime(f"[{Fore.CYAN}%H:%M{Style.RESET_ALL}]")
+time2 = datetime.now().strftime(f"{Fore.CYAN}%H:%M{Style.RESET_ALL}]")
 bot = ezcord.Bot(
     intents=intents,
-    language="de",
+    language="auto", default_language="de",
+    logging_level=logging.DEBUG,
     error_webhook_url=os.getenv("ERROR_WEBHOOK_URL"),
+    ready_event=None
 )
+@bot.event
+async def on_ready():
+    print(f"{time} [{Style.BRIGHT}{Fore.GREEN}STARTING UP{Style.RESET_ALL}] Logged in as {bot.user}")
+    await asyncio.sleep(1)
+    init_all()
+    await asyncio.sleep(1.5)
+    print(f"{time} [{Style.BRIGHT}{Fore.LIGHTCYAN_EX}API{Style.RESET_ALL}] Weather API has Loaded.")
+    await asyncio.sleep(1.5)
+    print(f"{time} [{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}FASTCODING{Style.RESET_ALL}] Fast is ready to use!")
+bot.add_help_command()
+
 if __name__ == "__main__":
-    bot.load_cogs("cogs", subdirectories=True)
+    # Cogs laden
+    bot.load_cogs("cogs", subdirectories=True, custom_log_level=f"{time2} [{Style.BRIGHT}{Fore.RED}COGS LOADING{Style.RESET_ALL}")
     bot.run(os.getenv("TOKEN"))
